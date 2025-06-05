427bdc83aa559ce5fed2e0c2bea3ed1cd4b1eec9
Felipe <felipe.w28@aluno.ifsc.edu.br>
2025-06-05 11:31:06 -0300

    O modelo cliente criado
    só faz o inserir

--- Atividade I6/LojaMVC/src/main/java/model/GenericDAO.java
+++ Atividade I6/LojaMVC/src/main/java/model/GenericDAO.java
@@ -1,26 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-import dal.ConexaoBD;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-/**
- *
- * @author Aluno
- */
+import dal.ConexaoBD;
+
+//Classe abstrata não pode ser instanciada, é um modelo para as classes que irão herdar seu comportamento
+//Esta classe facilita para que não precisemos em todas as Classes DAO executar todo este código, 
+//iremos apenas passar os parâmetros
 public abstract class GenericDAO {
+
     private Connection conexao;
-    
-    protected GenericDAO(){
-        this.conexao=ConexaoBD.conectar();
+
+    // Protected pois pertencem a esta classe, somente podem ser usadas por classes
+    // que herdam desta
+    protected GenericDAO() {
+        this.conexao = ConexaoBD.conectar();
     }
-    
-    protected Connection conectarDAO(){
+
+    // Método que retorna a conexaao
+    protected Connection conectarDAO() {
         this.conexao = ConexaoBD.conectar();
         return conexao;
     }
-    
+
+    // Método para salvar
+    protected void save(String insertSql, Object... parametros) throws SQLException {
+        PreparedStatement pstmt = conectarDAO().prepareStatement(insertSql);
+
+        for (int i = 0; i < parametros.length; i++) {
+            pstmt.setObject(i + 1, parametros[i]);
+        }
+
+        pstmt.execute();
+        pstmt.close();
+        conexao.close();
+    }
+
+    // Método para atualizar
+    protected void update(String updateSql, Object id, Object... parametros) throws SQLException {
+        PreparedStatement pstmt = conectarDAO().prepareStatement(updateSql);
+        for (int i = 0; i < parametros.length; i++) {
+            pstmt.setObject(i + 1, parametros[i]);
+        }
+        pstmt.setObject(parametros.length + 1, id);
+        pstmt.execute();
+        pstmt.close();
+        conexao.close();
+    }
+
+    // Método para deletar
+    protected void delete(String deleteSql, Object... parametros) throws SQLException {
+        PreparedStatement pstmt = conectarDAO().prepareStatement(deleteSql);
+
+        for (int i = 0; i < parametros.length; i++) {
+            pstmt.setObject(i + 1, parametros[i]);
+        }
+
+        pstmt.execute();
+        pstmt.close();
+        conexao.close();
+    }
 }
